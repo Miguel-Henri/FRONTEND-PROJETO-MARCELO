34361ea353dc50f5bb15a7b59b17af60f5b0889e
natanaraujo43 <natanaraujo3001@gmail.com>
2026-06-08 17:33:30 -0300

    criando telas do gerente e alterando algumas configs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-
 import { CadastroUsuarioComponent } from './pages/usuario/usuario.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TransferenciaComponent } from './pages/transferencia/transferencia.component';
@@ -7,51 +6,26 @@ import { DepositoComponent } from './pages/deposito/deposito.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { ExtratoComponent } from './pages/extrato/extrato.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { GerenteComponent } from './pages/gerente/gerente.component';
+import { gerenteGuard } from './core/guards/gerente.guard';
 
 export const routes: Routes = [
-
-  {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
-  },
-
-  {
-    path: 'usuario/cadastro',
-    component: CadastroUsuarioComponent
-  },
-
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'usuario/cadastro', component: CadastroUsuarioComponent },
   {
     path: '',
     component: MainLayoutComponent,
     children: [
-
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'transferencias', component: TransferenciaComponent },
+      { path: 'deposito', component: DepositoComponent },
+      { path: 'perfil', component: PerfilComponent },
+      { path: 'extrato', component: ExtratoComponent },
       {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-
-      {
-        path: 'transferencias',
-        component: TransferenciaComponent
-      },
-
-      {
-        path: 'deposito',
-        component: DepositoComponent
-      },
-
-      {
-        path: 'perfil',
-        component: PerfilComponent
-      },
-
-      {
-        path: 'extrato',
-        component: ExtratoComponent
+        path: 'gerente',
+        component: GerenteComponent,
+        canActivate: [gerenteGuard]   // ← protege a rota
       }
-
     ]
   }
-
 ];
